25159a6bdc2cdec09720665b528c80a07a887da7
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-11-29 19:29:49 +0300

    Add files via upload

--- SSQL/ssql.cpp
+++ SSQL/ssql.cpp
@@ -21,9 +21,16 @@ int main(){
 	string command_in_query ;
 	int  endpoint_in_query_POSITION;
 	int  name_of_table_POSITION;
+	// position of the end of query
 	endpoint_in_query_POSITION = query.find(";");
+	// position of the start of query
 	name_of_table_POSITION = query.find("!");  
+	//take command from the line;
+	command_in_query = query.substr(1,name_of_table_POSITION);
 
+	/*developing in the process*/
+	cout<<command_in_query<<endl;
+	cout<<query<<endl;
 
 
 	return 0;
