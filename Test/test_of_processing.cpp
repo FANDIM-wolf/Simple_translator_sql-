ea938f023202eca05944efbe80e1d485007ebb94
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-12-13 19:28:58 +0300

    Add files via upload

---
+++ Test/test_of_processing.cpp
@@ -0,0 +1,14 @@
+﻿"HelgaSQL.exe" (Win32). Загружено "C:\home\HelgaSQL\Debug\HelgaSQL.exe". Символы загружены.
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcp140d.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+Поток 0x1490 завершился с кодом 0 (0x0).
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". 
+"HelgaSQL.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". 
+Поток 0xbf4 завершился с кодом 0 (0x0).
+Поток 0x32fc завершился с кодом 0 (0x0).
+Программа "[11236] HelgaSQL.exe" завершилась с кодом 0 (0x0).
